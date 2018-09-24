21bb5df6d6489701ee9bc5a987c50ab77fda67dd
MrGuin <897705598@qq.com>
2018-09-24 18:20:32 +0800

    修改路由

--- src/routes.php
+++ src/routes.php
@@ -1,7 +1,7 @@
 <?php
-Route::group(['prefix'=>'dept', 'namespace'=>'Jxc\Controller'], function () {
-    Route::get('list', ['uses'=>'DeptController@deptList']);
-    Route::post('add', ['uses'=>'DeptController@addDept']);
-    Route::post('edit', ['uses'=>'DeptController@updateDept']);
-    Route::post('delete', ['uses'=>'DeptController@deleteDept']);
+Route::group(['prefix'=>'customer', 'namespace'=>'Jxc\Controller'], function () {
+    Route::get('list', ['uses'=>'CustomerController@customerList']);
+    Route::post('add', ['uses'=>'CustomerController@addCustomer']);
+    Route::post('edit', ['uses'=>'CustomerController@updateCustomer']);
+    Route::post('delete', ['uses'=>'CustomerController@deleteCustomer']);
 });
